68344887b2916c8eb8ffeac97bf046f58e288280
c-syerragu <c-syerragu@pa.gov>
2020-07-18 19:31:07 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          created a feature branch
         </a>
       </header>
     </div>
